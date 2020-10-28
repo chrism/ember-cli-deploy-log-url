2cc7400c1044e8a3144b328509304f4d8a717e3a
Chris Masters <chrismasters@gmail.com>
2020-10-28 18:54:57 +0100

    First attempt at logging to console after deployment

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 'use strict';
 
+var BasePlugin = require('ember-cli-deploy-plugin');
+
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = BasePlugin.extend({
+      name: options.name,
+
+      didDeploy: function(context) {
+        this.log('Successfully deployed', { color: 'green' });
+      }
+    });
+
+    return new DeployPlugin();
+  }
 };
